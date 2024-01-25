b5785c3f56876fed0a3572e115750df5afe3179e
Yahir Morfin <144406875+yahirmxx@users.noreply.github.com>
2024-01-24 20:12:00 -0600

    feature

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ app.get('/tasks', (req, res) => {
   res.json(tasks);
 });
 
+
+// Get all issues
+app.get('/issues', (req, res) => {
+  res.json(tasks);
+});
+
+
 // Get a specific task
 app.get('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
